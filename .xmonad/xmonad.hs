6a1a11c223122a4e9be857690d06939d6efa4ed6
Kristof Korwisi <kristof.korwisi@uni-wuerzburg.de>
2021-01-19 16:29:40 +0100

    Added bindings for volume control and customized volume bar

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,16 +2,26 @@ import XMonad
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.DynamicLog
 import XMonad.Util.Run(spawnPipe)
+import qualified XMonad.StackSet as W
+import XMonad.Util.EZConfig
 
 main = do
   xmprocs <- spawnPipe ("~/.cabal/bin/xmobar")
-  xmonad =<< xmobar def {
-    borderWidth = 2,
-    manageHook = myManageHook,
-    terminal = "alacritty",
-    normalBorderColor = "#cccccc",
-    focusedBorderColor = "#ffad00",
-    modMask = mod4Mask }
+  xmonad =<< xmobar myConfig
+
+myConfig = def {
+  borderWidth = 2,
+  manageHook = myManageHook,
+  terminal = "alacritty",
+  normalBorderColor = "#cccccc",
+  focusedBorderColor = "#ffad00",
+  modMask = mod4Mask } `additionalKeysP`
+  [ ("M-l", spawn "slock")
+  , ("M-e", spawn "emacs")
+  , ("M-<KP_Add>", spawn "pactl -- set-sink-volume 1 +5%")
+  , ("M-<KP_Subtract>", spawn "pactl -- set-sink-volume 1 -5%")
+  , ("M-<KP_Multiply>", spawn "pactl -- set-sink-mute 1 toggle")
+  ]
 
 myManageHook = composeAll
    [ className =? "Firefox" <&&> resource =? "Dialog"      --> doFloat,
